8214989ab84f192a17c764764bff233ea2503b23
Ba1tur <zbajtur@gmail.com>
2022-10-31 22:56:37 +0300

    first comment

---
+++ script.js
@@ -0,0 +1,24 @@
+// let strelock = {
+//     name: Oleg ,
+//     weapon: ak47 ,
+//     hp: 100 
+// }
+
+// let voin = {
+//     name: Alex ,
+//     weapon: sword ,
+//     hp: 100
+// }
+
+
+class Human {
+    name; 
+    hp;
+    constructor(name , hp) {
+    this.name =  name;
+    this.hp = hp; 
+   }
+   
+}
+
+let strelock = new Human('Человек',  '100')
